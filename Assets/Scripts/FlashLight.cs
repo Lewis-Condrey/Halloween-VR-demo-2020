7351f6e89d02efc34612b8074809fc73281a4a96
Lewis-Condrey <lewis-condrey@live.co.uk>
2020-10-21 02:00:28 +0100

    flashlight and styled hands

---
+++ Assets/Scripts/FlashLight.cs
@@ -0,0 +1,34 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+    public bool flashlightON;
+    public bool flashlightOFF;
+ 
+    public Light theLight;
+
+    private void Start()
+    {
+        theLight = GetComponentInChildren<Light>();
+        flashlightOFF = true;
+        flashlightON = false;
+        theLight.enabled = false;
+    }
+
+    public void LightON()
+    {
+        flashlightOFF = false;
+        flashlightON = true;
+        theLight.enabled = true;
+    }
+
+    public void LightOFF()
+    {
+        flashlightOFF = true;
+        flashlightON = false;
+        theLight.enabled = false;
+    }
+}
