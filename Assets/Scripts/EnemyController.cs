4ddc64be51287b3820cda7afa671107819f94916
Lewis-Condrey <lewis-condrey@live.co.uk>
2020-10-23 17:04:51 +0100

    ghoul implemented

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Rigidbody myrb;
+    public float moveSpeed;
+
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myrb = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        if (GameObject.Find("Player") != null)
+            myrb.velocity = (transform.forward * moveSpeed);
+        transform.LookAt(player.transform.position);
+    }
+
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Player")
+        {
+            Debug.Log("ded");
+        }
+    }
+}
