facfb38d17578a4b6ede3ab6bc87c19b5bb579ef
Lewis-Condrey <lewis-condrey@live.co.uk>
2020-10-30 19:39:47 +0000

    grab text

--- Assets/Scripts/ComputerText.cs
+++ Assets/Scripts/ComputerText.cs
@@ -4,23 +4,48 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.Experimental.XR;
 using UnityEngine.UI;
+using System;
+
 
 public class ComputerText : MonoBehaviour
 {
     public Text displayText;
-    XRGrabInteractable m_InteractableBase;
+    XRSimpleInteractable m_InteractableBase;
     //public string textToDisplay;
+    string userName;
 
 
     void Start()
     {
+        m_InteractableBase = GetComponent<XRSimpleInteractable>();
+        m_InteractableBase.onSelectEnter.AddListener(conClicked);
+
+        //string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+        string userName = (Environment.UserName);
+        Debug.Log(Environment.UserName);
+
         DisplayText();
         //displayText.text = "" + textToDisplay;
     }
 
     public void DisplayText() {
-       displayText.text = "What ever you want to display";
- }
+        //displayText.text = "What ever you want to display";
+
+        //displayText.text = (Environment.UserName);
+
+        //displayText.text = 
+    }
+
+    public void Clicked()
+    {
+        Debug.Log("Bönk");
+    }
+
+    void conClicked(XRBaseInteractor obj)
+    {
+        Debug.Log("Bönk");
+    }
+
 }
 
 
